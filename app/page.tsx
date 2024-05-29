5c39880d31c67c11abe07af1d9a32925bd2efb5b
netera-michael <54540339+netera-michael@users.noreply.github.com>
2024-05-30 01:31:46 +0300

    Loading pay button

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ const HomePage: React.FC = () => {
     apiKey: "",
   });
   const [amount, setAmount] = useState<number | null>(null);
+  const [loadingSubmit, setLoadingSubmit] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -53,6 +54,7 @@ const HomePage: React.FC = () => {
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
+    setLoadingSubmit(true);
     if (amount && selectedCurrency) {
       let conversionRate = 0;
       let currencyCode = "";
@@ -78,8 +80,6 @@ const HomePage: React.FC = () => {
       } else {
         amountInFils = conversionAmount * 100;
       }
-
-      // toast(`Amount requested: ${amount}\nCurrency requested: ${selectedCurrency}\nConversion: ${conversionAmount}`);
 
       // API call to Ziina
       const response = await fetch(
@@ -109,6 +109,7 @@ const HomePage: React.FC = () => {
         toast.error("Failed to create payment link");
       }
     }
+    setLoadingSubmit(false);
   };
 
   if (loading) {
@@ -200,8 +201,35 @@ const HomePage: React.FC = () => {
           <button
             type="submit"
             className="w-full px-4 py-2 font-bold text-black bg-white rounded-md hover:bg-gray-200"
+            disabled={loadingSubmit}
           >
-            Pay
+            {loadingSubmit ? (
+              <div className="flex items-center justify-center">
+                <svg
+                  className="w-5 h-5 mr-3 -ml-1 text-black animate-spin"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8v8H4z"
+                  ></path>
+                </svg>
+                Processing...
+              </div>
+            ) : (
+              "Pay"
+            )}
           </button>
         </form>
       </div>
